c5179bd6fa9038a614ff7d6d50e75df7a643ac33
unknown <micwilk2010@hotmail.fr>
2021-01-19 22:58:01 +0100

    ajoue rout simple  ejs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-const greeting : string = "hello world";
+import express from "express";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+
+let logger = require('morgan');
+
+import {mainPageRoute}  from "./routes/MainPageRoute";
+
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.set('view engine', 'ejs')
+app.use('/favicon.ico', express.static('../public/images'));
+
+app.use('/', mainPageRoute)
+
+
+app.listen(8989, () => {
+    console.log(`Server run to http://localhost:8989`);
+})
---
+++ src/routes/MainPageRoute.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { Request, Response } from 'express';
+
+const route: Router = Router();
+
+route.get('/', (req: Request, res: Response, next: () => void) => {
+    res.render('User') 
+    next()
+})
+
+export { route as mainPageRoute }
